b42569df8d0bb772e49688c3fa6aaf4c162415f1
KYUNGMINNA <nkm4095@naver.com>
2023-02-09 15:38:07 +0900

    # git 초기화

---
+++ NewJeans/src/main/java/com/example/NewJeans/entity/Comment.java
@@ -0,0 +1,39 @@
+package com.example.NewJeans.entity;
+
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "cmtID")
+@Builder
+
+@Entity
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long cmtID;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "boardID")
+    private Board boardId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "memID")
+    private Member memID;
+
+    private String cmtContent;
+
+    @CreationTimestamp
+    private LocalDateTime cmtDate;
+
+
+}
